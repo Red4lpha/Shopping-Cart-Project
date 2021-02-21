241b669bd1d0103133c4a4a20e67ba029a4f1f0c
Kevin S <alphalop42@gmail.com>
2021-02-20 16:34:11 -0800

    Addition of shop items array

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Shop = () => {
     return (
-        <div>
+        <main className="grid place-items-center bg-gray-100">
             <h1 className="text-7xl">SHOP</h1>
-        </div>
+            <section className="flex flex-row gap-5 py-10 w-1/2">
+                <h1 className="text-7xl">A1</h1>
+                <h1 className="text-7xl">A2</h1>
+                <h1 className="text-7xl">A3</h1>   
+            </section>
+        </main>
     )
 }
 
---
+++ src/components/ShopItems.js
@@ -0,0 +1,76 @@
+const ShopItems = [
+    {
+    name: "Nike Air Max 97 MSCHF x INRI Jesus Shoes",
+    image: "https://stockx-360.imgix.net/Nike-Air-Max-97-MSCHF-x-INRI-Jesus-Shoes/Images/Nike-Air-Max-97-MSCHF-x-INRI-Jesus-Shoes/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1607664149&w=1000",
+    description: "White Nike Air Max 97's filled with 60cc of holy water from the River Jordan. Please Note: Due to the condensation of the water in the air bubbles, tissue paper may come dampened.",
+    price: 1425,
+    },
+    {
+    name: "Nike Air Max 97 Cool Grey Baroque Brown",
+    image: "https://stockx-360.imgix.net/Nike-Air-Max-97-Cool-Grey-Baroque-Brown/Images/Nike-Air-Max-97-Cool-Grey-Baroque-Brown/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1603481985&w=1000",    
+    description: "Cool Grey/Baroque Brown-University Gold",
+    price: 170,
+    },
+    {    
+    name: "Nike Air Max 97 Black Red Silver",
+    image: "https://images.stockx.com/images/Nike-Air-Max-97-Black-Red-Silver.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&trim=color&updated_at=1607052703&w=1000",
+    description: "Black/Metallic Silver-University Red",
+    price: 160,
+    },
+    {    
+    name: "Nike Air Max 97 Cool Grey Baroque Brown",
+    image: "https://stockx-360.imgix.net/Nike-Air-Max-97-Cool-Grey-Baroque-Brown/Images/Nike-Air-Max-97-Cool-Grey-Baroque-Brown/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1603481985&w=1000",
+    description: "Cool Grey/Baroque Brown-University Gold",
+    price: 170,
+    },
+    {
+    name: "Jordan 1 Retro High Dark Mocha",
+    image: "https://stockx-360.imgix.net/Air-Jordan-1-Retro-High-Dark-Mocha/Images/Air-Jordan-1-Retro-High-Dark-Mocha/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1608736454&w=1000",
+    description: "Jordan Brand continued their Black Toe design theme in 2020 and released the Jordan 1 High Dark Mocha, now available on StockX. The Dark Mocha 1 was one of the most anticipated releases in 2020 due to its familiar colorblocking that referenced two of the greatest Jordan 1s of all-time, the Jordan 1 Travis Scott and the Jordan 1 Black Toe.",
+    price: 450,
+    },
+    {
+    name: "adidas Yeezy 500 Utility Black",
+    image: "https://stockx-360.imgix.net/adidas-Yeezy-500-Utility-Black/Images/adidas-Yeezy-500-Utility-Black/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606319127&w=1000",
+    description: "Kanye is back, this time with a brand new colorway of the Yeezy 500. Keeping it monochromatic, this pair is covered entirely in one color: utility black. Like other Yeezy 500s, this pair features a suede upper with mesh paneling. In addition, the midsole utilizes adidas’ Adiprene cushioning technology.",
+    price: 330,
+    },
+    {
+    name: "Jordan 1 Retro High Royal Toe",
+    image: "https://stockx-360.imgix.net/Air-Jordan-1-Retro-High-Black-Game-Royal-Toe/Images/Air-Jordan-1-Retro-High-Black-Game-Royal-Toe/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606322574&w=1000",
+    description: "Releases like the Shattered Backboard 1 and Bred Toe have shifted all eyes on Jordan’s “Black Toe” inspired colorways. The Royal Toe takes inspiration from the original Jordan 1 Royal colorway (which released in 1985) and adds design elements of the Black Toe 1 to create a familiar, but new dialogue.", 
+    price: 320,
+    },
+    {
+    name: "Jordan 5 Retro Alternate Bel-Air",
+    image: "https://stockx-360.imgix.net/Air-Jordan-5-Retro-Alternate-Bel-Air/Images/Air-Jordan-5-Retro-Alternate-Bel-Air/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606316558&w=1000",
+    description: "Also known as the Jordan 5 Poison Green, the Alternate Bel-Air consists of the same 90s style colors and patterns as its predecessor, only this time the grey upper is replaced with white leather. A multi-color midsole, icy translucent outsole, and mismatched green and pink Jumpman logos on the tongues complete this retro-style masterpiece. These sneakers released in August of 2020 and retailed for $200.",
+    price: 200,
+    },
+    {
+    name: "Nike Air Max 90 Infrared (2020)",
+    image: "https://stockx-360.imgix.net/Nike-Air-Max-90-Infrared-2020/Images/Nike-Air-Max-90-Infrared-2020/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1609366208&w=1000",
+    description: "The Nike Air Max 90 Infrared returned in its original form to celebrate the model’s 30th Anniversary. While younger Air Max fans might refer to the silhouette as the Air Max 90 Infrared, (referencing its release in 1990), the original colorway was dubbed Hyvent Orange. How the name shifted over time can’t be traced to one source directly, but it’s clear that the Infrared name stuck, and the rest is history.",
+    price: 180,
+    },
+    {
+    name: "Nike Air Rubber Dunk Off-White UNC",
+    image: "https://stockx-360.imgix.net/Nike-Air-Rubber-Dunk-Off-White-UNC/Images/Nike-Air-Rubber-Dunk-Off-White-UNC/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1609438911&w=1000",
+    description: "The Nike Air Rubber Dunk, designed by Off-White founder Virgil Abloh, touched down in 2020, debuting the designer’s first-ever signature sneaker with the sportswear giant. This UNC colorway is one of three from the pack.",
+    price: 330,
+    },
+    {
+    name: "Jordan 3 Retro SE Unite Fire Red",
+    image: "https://stockx-360.imgix.net/Air-Jordan-3-Retro-SE-Fire-Red/Images/Air-Jordan-3-Retro-SE-Fire-Red/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606318533&w=1000",
+    description: "This Jordan 3 is composed of a fire red tumbled leather upper with signature Elephant print overlays and a black rubber heel tab displaying “Nike Air” branding. Grey leather on the perforated eyelets, ankle collar, and smooth leather tongue with a red Jumpman logo completes the design.",
+    price: 220,
+    },
+    {
+    name: "Nike Air Force 1 Low Orange Skeleton",
+    image: "https://stockx-360.imgix.net/Nike-Air-Force-1-Low-Orange-Skeleton/Images/Nike-Air-Force-1-Low-Orange-Skeleton/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1609438989&w=1000",
+    description: "In 2018, Nike started to release Skeleton-themed Air Force 1s in October in celebration of Halloween. Two years later, the Swoosh continued the tradition and released another Air Force 1 Skeleton, only this time with a colorful new look.",
+    price: 200,
+    },
+];
+
+export default ShopItems;
