440bf38fa2aef6a95474e525334c6f732ab355d5
Kevin S <alphalop42@gmail.com>
2021-03-15 20:47:11 -0700

    Added functionality to ITEM count on the Order Summary section Added functionality to total item cost on the Order Summary section

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom';
 import CartCard from './CartCard'
+import ShopItems from './ShopItems';
 
 const Cart = (props) => {
     let length = props.cartItems.length;
+    const [totalCost, setTotalCost] = useState(0);
+
+    useEffect(() => {
+        let total = 0;
+        props.cartItems.map(item => {
+            total += ShopItems[item.itemIndex].price * item.quantity;
+            //console.log(total);
+        });
+        setTotalCost(total);
+    },[props.cartItems]);
     return (
         <div className="container mx-auto mt-10">
             <div className="flex shadow-md my-10">
@@ -22,16 +34,16 @@ const Cart = (props) => {
                         <CartCard item={item} index={index} handleRemove={props.handleRemove} 
                         handleIncrement={props.handleIncrement} handleDecrement={props.handleDecrement}/>        
                     ))}
-                    <a href="#" className="flex font-semibold text-indigo-600 text-sm mt-10">
+                    <NavLink to="/shop" className="flex font-semibold text-indigo-600 text-sm mt-10">
                         <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
                         Continue Shopping
-                    </a>
+                    </NavLink>
                 </section>
             <aside id="summary" className="bg-gray-300 w-1/4 px-8 py-10">
                 <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
                 <div className="flex justify-between mt-10 mb-5">
-                    <span className="font-semibold text-sm uppercase">Items 3</span>
-                    <span className="font-semibold text-sm">590$</span>
+                    <span className="font-semibold text-sm uppercase">Items {length}</span>
+                    <span className="font-semibold text-sm">${totalCost}</span>
                 </div>
                 <div>
                 <label className="font-medium inline-block mb-3 text-sm uppercase">Shipping</label>
