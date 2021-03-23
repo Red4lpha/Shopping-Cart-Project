968fd69fcc2c411a886d9645f704da768a96229d
Kevin S <alphalop42@gmail.com>
2021-03-22 21:09:28 -0700

    Minor css tweaks to CartCard.js

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -27,13 +27,13 @@ const CartCard = ({item, index, handleRemove, handleIncrement, handleDecrement})
                 <div className="flex flex-col justify-around ml-4 flex-grow">
                     <span className="text-blue-600 font-bold text-2x1">{name}</span>
                     {/* <span className="text-red-500 text-xs"></span> */}
-                    <div onClick={removeItem} className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</div>
+                    <div onClick={removeItem} className="cursor-pointer font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</div>
                 </div>
             </div>
             <span className="text-blue-600 text-center w-1/5 font-bold text-1xl">${price}</span>
 
             <div className="flex justify-center w-1/5">
-                <div onClick={decrement} className="bg-blue-700 px-2 py-2 rounded-full fill-current text-white">
+                <div onClick={decrement} className="bg-gray-300 px-2 py-2 rounded-full fill-current text-blue-700">
                     <svg className="w-3" 
                     viewBox="0 0 448 512">
                         <path 
@@ -41,7 +41,7 @@ const CartCard = ({item, index, handleRemove, handleIncrement, handleDecrement})
                     </svg>
                 </div>
                 <input className="mx-2 border text-center w-8" type="text" defaultValue={quantity} />
-                <div onClick={increment} className="bg-blue-700 px-2 py-2 rounded-full fill-current text-white">
+                <div onClick={increment} className="bg-gray-300 px-2 py-2 rounded-full fill-current text-blue-700">
                     <svg className="w-3" viewBox="0 0 448 512">
                         <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
                     </svg>
