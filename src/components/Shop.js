1ed5283a692e3ed4c6ddab7a7d724bc280aa8e39
Kevin S <alphalop42@gmail.com>
2021-03-23 20:30:28 -0700

    Minor tweaks

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -7,7 +7,7 @@ const Shop = (props) => {
 
     return (
         <main className="grid place-items-center bg-white">
-            <h1 className="text-7xl">SHOP</h1>
+            {/* <h1 className="text-7xl">SHOP</h1> */}
             <section className="flex flex-row flex-wrap justify-around gap-1 py-10 w-3/4">
                 {ShopItems.map((item, index) => (
                     <Card item={item} index={index} handleClick={props.handleClick} key={ubiqid()}/>
