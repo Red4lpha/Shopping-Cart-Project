a4cdac6e1c802319ec550bf34f0b57fed614cb7a
Kevin-Laptop <alphalop42@gmail.com>
2022-01-12 21:07:13 -0800

    Fixed home image from not showing up

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <main className="grid place-items-center bg-no-repeat bg-center bg-cover py-10h-screen"
-      aria-label='Designer Shoes Home Background Image'  
-      style={{
-        backgroundImage: `url("https://cdn.pixabay.com/photo/2015/10/29/01/24/shoes-1011596_960_720.jpg")`
-      }}>
-    </main>
+    <main className="grid place-items-center bg-no-repeat bg-center bg-cover py-10 h-screen"
+      style={{ backgroundImage: `url("https://cdn.pixabay.com/photo/2015/10/29/01/24/shoes-1011596_960_720.jpg")`}}
+      aria-label="Designer Shoes Home Background Image" />
   )
 }
 
